a8b0a23303810a11b9f289713a1a29719a288c63
Claudio Filho <claudiocsfilho@gmail.com>
2025-02-12 17:00:48 -0300

    First commit

---
+++ src/main/java/com/claudiocsfilho/desafio2_model_dominio_orm/Desafio2ModelDominioOrmApplication.java
@@ -0,0 +1,13 @@
+package com.claudiocsfilho.desafio2_model_dominio_orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio2ModelDominioOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio2ModelDominioOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/claudiocsfilho/desafio2_model_dominio_orm/Desafio2ModelDominioOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claudiocsfilho.desafio2_model_dominio_orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio2ModelDominioOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
